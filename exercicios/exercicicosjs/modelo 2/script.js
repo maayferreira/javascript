7534ba7979f398942b51f55627506409709e0e34
Mayara Ferreira <161524232+maayferreira@users.noreply.github.com>
2024-05-09 19:31:59 -0300

    atualização

--- exercicios/exercicicosjs/modelo 2/script.js
+++ exercicios/exercicicosjs/modelo 2/script.js
@@ -1,21 +1,7 @@
+funcion verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano + document.getElementsByName('txtano')
+    var res = documente.querySelector('div#res')
 
-function carregar() {
-var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('imagem')
-var data = new Date()
-var hora = data.getHours()
-msg.innerHTML = `Agora são ${hora}horas.` 
-if (hora >= 0 && hora < 12) {
-    //Bom dia 
-    img.src = '#'
-    document.body.style.background = '#e4cfa5'
-} else if (hora >= 12 && hora <= 18) {
-    //Boa tarde
-    img.src = '#'
-    document.body.style.background = '#314b5b'
-} else {
-    //Boa noite
-    img.src = '#'
-    document.body.style.background = '#02244f'
 }
-}  
